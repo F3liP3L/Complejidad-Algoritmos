b72e34251581c091469b9331f9fe0a2948fed642
juan.cardona <felipe7u7432@gmail.com>
2023-02-19 18:11:18 -0500

    Iniciando proyecto 🎉
    Creación del repositorio donde se subirán los ejercicios realizados en el curso de complejidad de algoritmos

---
+++ exercises/PacMan.py
@@ -0,0 +1,36 @@
+# Ejercicio 2451 de Becrowd PacMan
+
+def countMaxMeelConsume(road):
+    '''
+    Permite contar la mayor cantidad de comida consumida en el recorrido.
+    Args:
+        str road
+    Returns:
+        int
+    '''
+    maxInsuredConsume = 0
+    mealInsuredConsume = 0
+    for letter in road:
+        if (letter == 'A'):
+            maxInsuredConsume = max(maxInsuredConsume, mealInsuredConsume)
+            mealInsuredConsume = 0
+        elif (letter == 'o'):
+            mealInsuredConsume += 1
+    maxInsuredConsume = max(maxInsuredConsume, mealInsuredConsume) 
+    return maxInsuredConsume
+
+
+pacmanRoadReverse = ""
+pacmanRoadTotal = ""
+try:
+    boardLength = int(input('Elige el tamaño del recorrido horizontal: '))
+    for i in range(boardLength):
+        pacmanRoad = input()[0:boardLength] #Se toma solo la cantidad de letras requeridas.
+        if(i % 2 != 0):
+            pacmanRoad = pacmanRoad[::-1] #Se invierte la cadena.
+        pacmanRoadTotal += pacmanRoad
+except Exception:
+    print("An unexpected error!!")
+finally:
+    print(str(countMaxMeelConsume(pacmanRoadTotal))+"\n")
+
